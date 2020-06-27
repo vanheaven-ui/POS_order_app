ba3e18d928526b06f5371a0a26bfa31afe4e01fe
vanheaven <vanheaven6@gmail.com>
2020-06-27 10:11:29 +0300

    Update validate method

--- lib/user.rb
+++ lib/user.rb
@@ -1,18 +1,21 @@
 module User 
-VALUE = %w[y n]
-  def User.welcome
+  def self.welcome
     puts 'Hey! Welcome to food haven'
     puts 'Care to share your name'
-    puts "Glad to have you #{gets.chomp.capitalize}"
+    User.validate
     puts "\n"
   end
 
-  def User.validate(input)
+  def self.validate(name)
     confirm = true
     while confirm
-      puts 'Please enter y or n to confirm' unless VALUE.include? input
+      name = gets.chomp.capitalize
+      puts 'Kindly enter your awesome name to proceed' unless name.size >= 3
 
-      break
+      confirm = false if name.size >= 3
     end
+    puts "Glad to have you #{name}"
   end
 end
+
+User.name
