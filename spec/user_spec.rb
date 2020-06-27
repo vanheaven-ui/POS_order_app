d1664cfe95e7b4fb75c5033bff74e2cc7c8de01e
vanheaven <vanheaven6@gmail.com>
2020-06-27 10:25:50 +0300

    Update rspec test for User.validate module cmethod

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,12 +3,11 @@ require_relative '../lib/user.rb'
 describe User do
   describe '.validate' do
     context 'User to enter full name' do
-      name = gets.chomp.capitalize
       it 'returns enter your awesome name to proceed' do
-        expect(User.validate(name)).to eql('Enter your awesome name to proceed')
+        expect(User.validate(gets.chomp.capitalize)).to eql('Enter your awesome name to proceed')
       end
       it 'returns Glad to have you <capitalized name>' do
-        expect(User.validate(name)).to eql('Glad to have you Ezekiel')
+        expect(User.validate(gets.chomp.capitalize)).to eql('Glad to have you Ezekiel')
       end
     end
   end
