b41e1744ea7b2becb209164b5ea6edbd015e7225
vanheaven <vanheaven6@gmail.com>
2020-07-01 22:22:14 +0300

    Update user_spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,11 +3,11 @@ require_relative '../lib/user.rb'
 describe User do
   describe '.validate' do
     context 'User to enter full name' do
-      it 'returns Kindly enter your awesome name to proceed' do
+      it 'returns kindly enter your awesome name in full to proceed' do
         expect(User.validate('Ez')).to eql('Enter your awesome name to proceed')
       end
       it 'returns Glad to have you <capitalized name>' do
-        expect(User.validate(gets.chomp.capitalize)).to eql('Glad to have you Ezekiel')
+        expect(User.validate('Ezekiel')).to eql('Glad to have you Ezekiel')
       end
     end
   end
