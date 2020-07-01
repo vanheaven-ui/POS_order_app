2eabd17a3d3e617d51a9c7e7298b81b9fd4f9144
vanheaven <vanheaven6@gmail.com>
2020-07-01 21:33:39 +0300

    Add validate, remove, and confirm validation methods

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,5 @@
-module User 
+module User
+  VALUE = %w[y n].freeze
   def self.welcome
     puts 'Hey! Welcome to food haven'
     puts 'Care to share your name'
@@ -6,16 +7,34 @@ module User
     puts "\n"
   end
 
-  def self.validate(name)
+  def self.validate
     confirm = true
     while confirm
       name = gets.chomp.capitalize
-      puts 'Kindly enter your awesome name to proceed' unless name.size >= 3
+      puts 'Kindly enter your awesome name in full to proceed' unless name.size >= 3
 
       confirm = false if name.size >= 3
     end
     puts "Glad to have you #{name}"
   end
-end
 
-User.name
+  def self.confirm
+    check = true
+    while check
+      $response = gets.chomp.downcase
+      puts 'Enter y or n to confirm' unless VALUE.include? $response
+
+      check = false if VALUE.include? $response
+    end
+  end
+
+  def self.remove(order)
+    remove = true
+    while remove
+      $discard = gets.chomp.capitalize
+      puts "#{$discard} does not match entry in your cart, enter again" unless order.include?($discard)
+
+      remove = false if order.include?($discard)
+    end
+  end
+end
